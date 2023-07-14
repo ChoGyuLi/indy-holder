991abb38845a8c8e0de8d4941dcc82088e074459
seooony <tjgus21127@gmail.com>
2023-07-14 17:55:31 +0900

    7/14

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.app;
 
-import android.app.Application;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.hyperledger.indy.sdk.IndyException;
+import org.hyperledger.indy.sdk.pool.Pool;
 
 import kr.co.bdgen.indywrapper.IndyWrapper;
+import kr.co.bdgen.indywrapper.config.PoolConfig;
 
-public class MainActivity extends Application {
+public class MainActivity extends AppCompatActivity {
     @Override
-   public void onCreate() {
-       super.onCreate();
-       IndyWrapper.init(this);
-   }
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        IndyWrapper.init(this);
+        String poolName = PoolConfig.getPoole(this);
+        try {
+            Pool.openPoolLedger(poolName, "{}").get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
